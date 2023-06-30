1cc33f4d14f5bff6df71c137993d825cd1b20018
Supardi G <pardiwan267@gmail.com>
2023-06-30 21:10:50 +0700

    initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import solidJs from "@astrojs/solid-js";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [solidJs(), tailwind()],
+  experimental: {
+    assets: true,
+  },
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "theme-bg": "#121212",
+        "theme-text": "#8D8A8A",
+      },
+      fontFamily: {
+        NeueHaasDisplay: ["NeueHaasDisplay", ...defaultTheme.fontFamily.sans],
+        Inter: ["Inter", ...defaultTheme.fontFamily.sans],
+        PlusJakartaSans: ["Plus Jakarta Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
