45335c5774f9a51f134a25da6bbfb8e62b33d945
Supardi G <pardiwan267@gmail.com>
2023-07-01 00:52:57 +0700

    add blog

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from "astro:content";
+
+const posts = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    createdAt: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    image: z.string(),
+  }),
+});
+
+export const collections = { posts };
