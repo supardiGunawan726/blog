8af10c3ce8b78f71ca0b69398d881d43f8d200b9
Supardi G <pardiwan267@gmail.com>
2023-07-07 16:28:27 +0700

    fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.supardi.dev",
   integrations: [solidJs(), tailwind()],
   experimental: {
     assets: true,
