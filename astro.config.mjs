b937a0ed5fa0df1dce98f8a7ce66e4624d6c99da
Supardi G <pardiwan267@gmail.com>
2023-07-07 16:45:13 +0700

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.supardi.dev",
-  integrations: [solidJs(), tailwind()],
+  integrations: [solidJs(), tailwind(), sitemap()],
   experimental: {
-    assets: true,
-  },
+    assets: true
+  }
 });
